4785587b6bfb427719abef527f83feb99e3bea4d
Robin Graf <robingraf@hotmail.co.uk>
2018-05-08 21:29:47 +0100

    added weather
    added weather components and weather class

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ my-app/src/app/weather.ts
@@ -0,0 +1,5 @@
+export class Weather {
+	location: string;
+	temperature: number;
+
+}
